8d68c2762183440fe6a762abdab28f9202e9bef6
Parag Bangad <paragbangad@hotmail.com>
2017-06-29 22:44:51 +0530

    integrationTests

---
+++ IntegrationTestProject/IntegrationTestProject/IntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntegrationTestProject
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod]
+        public void VerifyMultiply()
+        {
+            var srv = new sRefCalc.CalcClient();
+            Assert.AreEqual(srv.MultiplyNumbers(1, 2), 10.2);
+        }
+    }
+}
---
+++ IntegrationTestProject/IntegrationTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntegrationTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntegrationTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2728f9f6-6730-4cf5-9679-962501dcc61f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
