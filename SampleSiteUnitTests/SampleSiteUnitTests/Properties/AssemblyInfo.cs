eb06a856ab0febbd4edbd956551d2b430c711234
Parag Bangad <paragbangad@hotmail.com>
2017-06-29 16:00:58 +0530

    Removed WCF solution, added unit test solution for website

---
+++ SampleSiteUnitTests/SampleSiteUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SampleSiteUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleSiteUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ad7c72d-72aa-4fe3-b3b5-9eb6f8600cde")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WCFService/App_Code/Calc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Calc" in code, svc and config file together.
+public class Calc : ICalc
+{
+    public double AddNumbers(double number1, double number2)
+    {
+        return 10.0;
+    }
+
+    public double DivisionNumbers(double number1, double number2)
+    {
+        return 10.1;
+    }
+
+    public double MultiplyNumbers(double number1, double number2)
+    {
+        return 10.2;
+    }
+
+    public double SubstractNumbers(double number1, double number2)
+    {
+        return 10.2;
+    }
+}
