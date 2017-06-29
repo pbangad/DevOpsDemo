d701f9ff3991d6cfefa8550e7ca3e934129f1357
Parag Bangad <paragbangad@hotmail.com>
2017-06-29 19:00:16 +0530

    Unit Tests

---
+++ SampleSiteUnitTests/SampleSiteUnitTests/CalcTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo.SampleSiteUnitTests
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void AssertAddNumber()
+        {
+            var calc = new Calc();
+            Assert.AreEqual(calc.AddNumbers(1, 2), 10.0);
+        }
+    }
+    [TestClass]public class Sitetest{[TestMethod] public void AssertButtonInvisible() { Assert.AreEqual(0, 0); } }
+}
