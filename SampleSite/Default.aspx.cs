3bd7517da9b4f8b8ff7bfb6a4653761a12141a32
Parag Bangad <paragbangad@hotmail.com>
2017-06-29 12:40:11 +0530

    SampleSite and Calc Code

---
+++ SampleSite/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BTN1_Click(object sender, EventArgs e)
+    {
+        TXT1.Text = "Test Text";
+        BTN2.Visible = true;
+    }
+
+    protected void BTN2_Click(object sender, EventArgs e)
+    {
+        TXT1.Text = "";
+        BTN2.Visible = false;
+    }
+}
